e278ebb449e3590ddc01a963f6a3022c59dfee58
Russell Feldhausen <russfeldh@gmail.com>
2026-05-29 14:56:54 +0000

    Update Examples & Bump Docker to 24

--- examples/provider/src/configs/lti.js
+++ examples/provider/src/configs/lti.js
@@ -28,8 +28,8 @@ const lti = await LTIToolkit({
     handleLaunch: LTILaunch,
     // // LTI 1.0 Consumer Key and Shared Secret
     // // for single LTI consumer setup
-    // key: process.env.LTI_CONSUMER_KEY,
-    // secret: process.env.LTI_CONSUMER_SECRET,
+    key: process.env.LTI_CONSUMER_KEY,
+    secret: process.env.LTI_CONSUMER_SECRET,
     // Enable Deeplinking
     handleDeeplink: LTIDeepLink,
     // Enable Course Navigation Link
